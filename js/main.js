4680879c413a05facc6dca2755bc9a43fdf0c4d3
Rasulwebs <boboqulovrasul80@gamil.com>
2022-09-09 06:42:09 +0500

    jsmasalamodul_3

---
+++ js/main.js
@@ -0,0 +1,78 @@
+"use strict";
+
+//  ============================ 1 masala =========================
+
+// const array = ['a', 'a', 'a', 'a', 'a', 1, 1, 1, 1, 3, 3, 5, 5, 5, 5, 7, 7, ]
+// let arr = [];
+
+// for (let i = 0; i < array.length; i++){
+   
+// }
+//  array.filter((item) => {
+//      if (item == item) {
+//          console.log(item);
+//      }
+//  })
+
+
+
+// ================ 2 masala ==========================
+
+// let a=+prompt("a sonn");
+// let b=+prompt("b son");
+// let arr=[];
+
+// for(let i=a; i<=b; i++){
+// arr.push(i)
+
+// }
+// console.log(arr);
+
+
+
+//  ============= 3 masala ====================
+
+// let lol = +prompt("x son");
+// let yeh = +prompt("y son");
+// let text = [];
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < array.length; i++) {
+//   if (lol === i) {
+//     continue;
+//   } else if (yeh === i) {
+//     continue;
+//   } else {
+//     console.log();
+//   }
+//   console.log(array[i]);
+// }
+
+//  =============== 4 masala ===================
+
+// let array = [
+//     {
+//       isname: "Nemo",
+//       age: 17,
+//       jop: "Programm",
+//     },
+//     {
+//       isname: "Lil",
+//       age: 27,
+//       jop: "Musical",
+//     },
+//     {
+//       isname: "Tom",
+//       info: "Run",
+//       jop: "aviator",
+//     },
+//   ];
+//   let x=[]
+//   array.forEach((e) => {
+//     for (let key in e) {
+//      x.push(e[key])
+//    }
+//   });
+//   console.log(x);
+
+
